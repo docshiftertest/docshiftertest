0d21deae5f83c255a93c1c9380bd7ba35d2d2905
Michiel Van Driessche <Michiel.VanDriessche@docbyte.com>
2018-07-02 14:58:40 +0200

    changed asserts for Codacy and removed some unused code

--- src/test/java/com/docshifter/core/messaging/DateDeserializerTest.java
+++ src/test/java/com/docshifter/core/messaging/DateDeserializerTest.java
@@ -10,8 +10,8 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Date;
 
-import static org.junit.Assert.assertEquals;
-import static org.springframework.util.Assert.*;
+import static org.junit.Assert.*;
+import static org.springframework.util.Assert.isInstanceOf;
 
 @SpringBootTest
 public class DateDeserializerTest {
@@ -22,7 +22,7 @@ public class DateDeserializerTest {
 		DateDeserializer dateDeserializer = new DateDeserializer();
 		Object dateObj =dateDeserializer.deserializeDate("notadate");
 		
-		isNull(dateObj);
+		assertNull(dateObj);
 	}
 	
 	@Test
@@ -30,7 +30,7 @@ public class DateDeserializerTest {
 		DateDeserializer dateDeserializer = new DateDeserializer();
 		Object dateObj = dateDeserializer.deserializeDate("12345778");
 		
-		isNull(dateObj);
+		assertNull(dateObj);
 	}
 	
 	/**
@@ -40,7 +40,7 @@ public class DateDeserializerTest {
 	public void deserializeShouldParse1() throws Exception {
 		DateDeserializer dateDeserializer = new DateDeserializer();
 		Object dateObj = dateDeserializer.deserializeDate("2017-11-27T15:49:37+01:00");
-		notNull(dateObj);
+		assertNotNull(dateObj);
 		isInstanceOf(Date.class, dateObj);
 		Instant date = ((Date) dateObj).toInstant();
 		ZonedDateTime zdt = ZonedDateTime.ofInstant(date, ZoneId.systemDefault());
@@ -60,7 +60,7 @@ public class DateDeserializerTest {
 	public void deserializeShouldParse2() throws Exception {
 		DateDeserializer dateDeserializer = new DateDeserializer();
 		Object dateObj = dateDeserializer.deserializeDate("1991-09-27T01:05:32+02:00");
-		notNull(dateObj);
+		assertNotNull(dateObj);
 		isInstanceOf(Date.class, dateObj);
 		
 		Instant date = ((Date) dateObj).toInstant();
@@ -80,7 +80,7 @@ public class DateDeserializerTest {
 	public void deserializeShouldParse3() throws Exception {
 		DateDeserializer dateDeserializer = new DateDeserializer();
 		Object dateObj = dateDeserializer.deserializeDate("1991-09-27T01:05:32Z");
-		notNull(dateObj);
+		assertNotNull(dateObj);
 		isInstanceOf(Date.class, dateObj);
 		
 		Instant date = ((Date) dateObj).toInstant();
@@ -92,22 +92,6 @@ public class DateDeserializerTest {
 		assertEquals(1, zdt.getHour());
 		assertEquals(5, zdt.getMinute());
 		assertEquals(32, zdt.getSecond());
-		
-	}
-	
-	
-	
-	public void serializedate() throws Exception {
-		ZonedDateTime zdt = ZonedDateTime.of(2017, 11, 27, 15, 49, 37, 0,ZoneId.systemDefault());
-		ZonedDateTime zdt2 = ZonedDateTime.of(1991, 11, 27, 1, 49, 37, 0,ZoneId.systemDefault());
-		
-		final DateFormat formatter = new ISO8601DateFormat();
-		
-		System.out.println(zdt.toString());
-		System.out.println(zdt2.toString());
-		System.out.println(formatter.format(Date.from(zdt.toInstant())));
-		System.out.println(formatter.format(Date.from(zdt2.toInstant())));
-		
 		
 	}
 	
