37e05f5a98cdee288c8a6f065db85ba72753612d
Juan Marques <juan.marques@docshifter.com>
2020-10-08 13:17:28 +0200

    TaskData refactoring.

--- src/main/java/com/docshifter/core/TaskDataConsts.java
+++
@@ -1,8 +0,0 @@
-package com.docshifter.core;
-
-public class TaskDataConsts {
-	private TaskDataConsts() {}
-
-	public static final String DOCUMENT_BOUNDARY_TEXT = "document_boundary_text";
-	public static final String ADD_BOUNDARY_TITLE = "add_boundary_title";
-}
