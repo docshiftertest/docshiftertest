57a7971568ab452c81dc45238884da19813c14c9
Johnno Van de Velde <johnno.vandevelde@docshifter.com>
2020-10-08 08:45:12 +0200

    Make boundary title configurable too

--- src/main/java/com/docshifter/core/TaskDataConsts.java
+++ src/main/java/com/docshifter/core/TaskDataConsts.java
@@ -3,5 +3,6 @@ package com.docshifter.core;
 public class TaskDataConsts {
 	private TaskDataConsts() {}
 
-	public static final String BOUNDARY_BOOKMARK_TEXT = "boundary_bookmark_text";
+	public static final String DOCUMENT_BOUNDARY_TEXT = "document_boundary_text";
+	public static final String ADD_BOUNDARY_TITLE = "add_boundary_title";
 }
