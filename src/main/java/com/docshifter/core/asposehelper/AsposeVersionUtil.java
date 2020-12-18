9f40bde91bb25e4386b6bad8e2de17c6ae2042c9
julian-ds <julian.isaac@docshifter.com>
2020-12-18 09:11:59 +0100

    The big reorg...continued...update to Aspose licensing
    Hold lic file as a b64 encoded String in the LicenseHelper and make a temp file from it to create the licences. Update Version util to cope with ocr version. Add a log4j so we can see some debugs in testing? Bump ocr to 20.11 and diagram to 20.12 (fixes regression)

--- src/main/java/com/docshifter/core/asposehelper/AsposeVersionUtil.java
+++ src/main/java/com/docshifter/core/asposehelper/AsposeVersionUtil.java
@@ -1,14 +1,23 @@
 package com.docshifter.core.asposehelper;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.jar.Attributes;
 import java.util.jar.Attributes.Name;
+import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 /**
  * Gets the version of an aspose lib both according to the version specified in the
@@ -29,7 +38,7 @@ public class AsposeVersionUtil {
 		asposeMappings.put("pdf", com.aspose.pdf.Document.class);
 		asposeMappings.put("imaging", com.aspose.imaging.Image.class);
 		asposeMappings.put("tasks", com.aspose.tasks.Task.class);
-		asposeMappings.put("ocr", com.aspose.ocr.OcrEngine.class);
+		asposeMappings.put("ocr", com.aspose.ocr.AsposeOCR.class);
 		asposeMappings.put("diagram", com.aspose.diagram.Diagram.class);
 		asposeMappings.put("cad", com.aspose.cad.Image.class);
 		asposeMappings.put("html", com.aspose.html.HTMLDocument.class);
@@ -70,7 +79,7 @@ public class AsposeVersionUtil {
 	 */
 	public static String getActualVersion(String className) {
 		Class<?> clazz = getAsposeClass(className);
-		return getActualVersion(clazz);
+		return getActualVersion(clazz, className);
 	}
 
 	/**
@@ -100,13 +109,16 @@ public class AsposeVersionUtil {
 	 * @param clazz An aspose Class
 	 * @return String the version actually running or Unknown: and the text of an exception
 	 */
-	private static String getActualVersion(Class<?> clazz) {
+	private static String getActualVersion(Class<?> clazz, String libName) {
 		String result = "Unknown: ";
 		URL urlFromDomain = clazz.getProtectionDomain().getCodeSource().getLocation();
 		Attributes attributes = null;
 		try (JarFile jar = new JarFile(urlFromDomain.toURI().getPath())) {
 			attributes = jar.getManifest().getMainAttributes();
 			result = String.valueOf(attributes.getOrDefault(IMPLEMENTATION_VERSION, "undefined"));
+			if (result.equals("undefined")) {
+				result = getActualVersionAlternative(jar, libName);
+			}
 		}
 		catch (Exception exc) {
 			result += exc;
@@ -114,12 +126,29 @@ public class AsposeVersionUtil {
 		return result;
 	}
 
-	/**
-	 * Gets a property from the given property file
-	 * @param propertyFile the name of the property file, including .properties suffix
-	 * @param propertyName the property you're looking for
-	 * @return String either the property or Unknown: and the text of an exception
-	 */
+	private static String getActualVersionAlternative(JarFile jar, String libName) throws IOException {
+		String result = "undefined";
+		JarEntry entry = jar.getJarEntry("META-INF/maven/com.aspose/aspose-"
+				+ libName + "/pom.properties");
+		if (entry != null) {
+			String content = IOUtils.toString(jar.getInputStream(entry), StandardCharsets.UTF_8);
+			if (StringUtils.isNotBlank(content)) {
+				Properties props = new Properties();
+				props.load(new StringReader(content));
+				if (props.containsKey("version")) {
+					result = (String) props.get("version");
+				}
+			}
+		}
+		return result;
+	}
+
+		/**
+         * Gets a property from the given property file
+         * @param propertyFile the name of the property file, including .properties suffix
+         * @param propertyName the property you're looking for
+         * @return String either the property or Unknown: and the text of an exception
+         */
 	public static String getProperty(String propertyFile, String propertyName) {
 		String result = "Unknown: ";
 		try (InputStream is = ClassLoader.getSystemResourceAsStream(propertyFile)) {
--- src/main/java/com/docshifter/core/asposehelper/LicenseHelper.java
+++ src/main/java/com/docshifter/core/asposehelper/LicenseHelper.java
@@ -1,24 +1,51 @@
 package com.docshifter.core.asposehelper;
 
-/**
- * Created by samnang.nop on 21/02/2017.
- */
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import org.apache.log4j.Logger;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
+import java.util.Base64;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
-public final class LicenseHelper {
+public class LicenseHelper {
 
-	private static final Logger logger = LoggerFactory.getLogger(LicenseHelper.class);
 	private static final LicenseHelper licenseHelper = new LicenseHelper();
+	private static final Logger logger = Logger.getLogger(LicenseHelper.class);
+
+	private static String B64_5 =  "ZW5zZVR5cGU+RGV2ZWxvcGVyIE9FTTwvTGljZW5zZVR5cGU+DQ";
+	private static String B64_P =  "RUVPN0R1N3JZUHNJTXo5aXFBeWVScjI0Rzk3WUY4SlZhODZuTi";
+	private static String B64_M =  "AgPFNpZ25hdHVyZT53YmxPWkJLZ3NRcDR3dElCZkErbFdhcTdi";
+	private static String B64_4 =  "hlbUBkb2NzaGlmdGVyLmNvbTwvRW1haWxUbz4NCiAgICA8TGlj";
+	private static String B64_2 =  "RhdGE+DQogICAgPExpY2Vuc2VkVG8+RG9jU2hpZnRlcjwvTGlj";
+	private static String B64_A =  "AgPE9FTT5UaGlzIGlzIGEgcmVkaXN0cmlidXRhYmxlIGxpY2Vu";
+	private static String B64_N =  "blRtUnc0V1VadWc2Uk1JVklRRlZSbng5SnNoTXZPVjlka3VWem";
+	private static String B64_Q =  "tuZXErVWUrUjU2elQ5RDMzTDlvUGpIb0tXekZJWmZiUmc9PC9T";
+	private static String B64_8 =  "5vdGU+DQogICAgPE9yZGVySUQ+MjAxMTIwMTE1NjI5PC9PcmRl";
+	private static String B64_I =  "AgICA8TGljZW5zZVZlcnNpb24+My4wPC9MaWNlbnNlVmVyc2lv";
+	private static String B64_O =  "JaV1RIeW5BdTU1VTdOV3Y3Y0YxRFBvbXc1alVXRVo1emZMOHJB";
+	private static String B64_6 =  "ogICAgPExpY2Vuc2VOb3RlPk9uZSBEZXZlbG9wZXIgQW5kIFVu";
+	private static String B64_H =  "eHBpcnk+MjAyMTEyMDM8L1N1YnNjcmlwdGlvbkV4cGlyeT4NCi";
+	private static String B64_L =  "c2U8L0xpY2Vuc2VJbnN0cnVjdGlvbnM+DQogIDwvRGF0YT4NCi";
+	private static String B64_1 =  "PD94bWwgdmVyc2lvbj0iMS4wIj8+DQo8TGljZW5zZT4NCiAgPE";
+	private static String B64_D =  "ICAgPC9Qcm9kdWN0cz4NCiAgICA8RWRpdGlvblR5cGU+UHJvZm";
+	private static String B64_7 =  "bGltaXRlZCBEZXBsb3ltZW50IExvY2F0aW9uczwvTGljZW5zZU";
+	private static String B64_F =  "YmVyPjU4YmZmZjU2LTQ3ODQtNDBhZS1iZGM5LWE5NjJkNDRmNm";
+	private static String B64_C =  "VjdD5Bc3Bvc2UuVG90YWwgZm9yIEphdmE8L1Byb2R1Y3Q+DQog";
+	private static String B64_K =  "B1cmNoYXNlLmFzcG9zZS5jb20vcG9saWNpZXMvdXNlLWxpY2Vu";
+	private static String B64_G =  "E0NDwvU2VyaWFsTnVtYmVyPg0KICAgIDxTdWJzY3JpcHRpb25F";
+	private static String B64_9 =  "cklEPg0KICAgIDxVc2VySUQ+ODQwMDE1PC9Vc2VySUQ+DQogIC";
+	private static String B64_B =  "c2U8L09FTT4NCiAgICA8UHJvZHVjdHM+DQogICAgICA8UHJvZH";
+	private static String B64_3 =  "ZW5zZWRUbz4NCiAgICA8RW1haWxUbz5nZWVydC52YW5wZXRlZ2";
+	private static String B64_E =  "Vzc2lvbmFsPC9FZGl0aW9uVHlwZT4NCiAgICA8U2VyaWFsTnVt";
+	private static String B64_J =  "bj4NCiAgICA8TGljZW5zZUluc3RydWN0aW9ucz5odHRwczovL3";
+	private static String B64_R =  "aWduYXR1cmU+DQo8L0xpY2Vuc2U+";
+	private static String B64 =
+			B64_1 + B64_2 + B64_3 + B64_4 + B64_5 + B64_6 + B64_7 + B64_8 + B64_9 +
+					B64_A + B64_B + B64_C + B64_D + B64_E + B64_F + B64_G + B64_H + B64_I +
+					B64_J + B64_K + B64_L + B64_M + B64_N + B64_O + B64_P + B64_Q + B64_R;
+
 	private final com.aspose.words.License wordsLicense;
 	private final com.aspose.cells.License cellsLicense;
 	private final com.aspose.slides.License slidesLicense;
@@ -30,39 +57,18 @@ public final class LicenseHelper {
 	private final com.aspose.diagram.License diagramLicense;
 	private final com.aspose.cad.License cadLicense;
 
-
-	private static final String lic = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-			"<License>\n" +
-			"<Data>\n" +
-			"<LicensedTo>DocShifter</LicensedTo>\n" +
-			"<EmailTo>geert.vanpeteghem@docshifter.com</EmailTo>\n" +
-			"<LicenseType>Developer OEM</LicenseType>\n" +
-			"<LicenseNote>Limited to 1 developer, unlimited physical locations</LicenseNote>\n" +
-			"<OrderID>191125041947</OrderID>\n" +
-			"<UserID>135031188</UserID>\n" +
-			"<OEM>This is a redistributable license</OEM>\n" +
-			"<Products>\n" +
-			"<Product>Aspose.Total for Java</Product>\n" +
-			"</Products>\n" +
-			"<EditionType>Enterprise</EditionType>\n" +
-			"<SerialNumber>9fd10bea-38f9-4030-aec6-4050242f483d</SerialNumber>\n" +
-			"<SubscriptionExpiry>20201203</SubscriptionExpiry>\n" +
-			"<LicenseVersion>3.0</LicenseVersion>\n" +
-			"<LicenseInstructions>https://purchase.aspose.com/policies/use-license</LicenseInstructions>\n" +
-			"</Data>\n" +
-			"<Signature>YkLNPD+g9khxOs+7ForIVarr3pWCXoSrxA+gBtJwJJuXCAsilcDMpraRluycDmmAl0jBnFNasUgtnEokxAh9JwsDtHYkrfH5pyApnc8mixWa6YePPRmwT7ld5h5norU+2ycDmDz2s/890Vlt1qy6x/oAU3hEMtT1x8f/+UkhtUI=</Signature>\n" +
-			"</License>";
-
-	public static void main(String[] arghs) {
-		if (arghs.length > 0) {
-			LicenseHelper.licenseReader(Paths.get(arghs[0]));
+	public LicenseHelper() {
+		byte[] licenceBytes = Base64.getDecoder().decode(B64.getBytes(StandardCharsets.UTF_8));
+		String tmpFileName = UUID.randomUUID().toString();
+		String tmpDir = System.getProperty("java.io.tmpdir");
+		Path tmpFileFullPath = Paths.get(tmpDir, tmpFileName);
+		String tmpFileFullPathStr = tmpFileFullPath.toString();
+		try {
+			Files.write(tmpFileFullPath, licenceBytes);
 		}
-		else {
-			System.out.println("You have to give a lic file path as an argument if you want to use the handy lic file reader!");
+		catch (IOException ioe) {
+			logger.error("Trying to write to: " + tmpFileFullPath + ", we hit an IOException", ioe);
 		}
-	}
-	
-	private LicenseHelper() {
 
 		emailLicense = new com.aspose.email.License();
 		slidesLicense = new com.aspose.slides.License();
@@ -74,29 +80,24 @@ public final class LicenseHelper {
 		ocrLicense = new com.aspose.ocr.License();
 		diagramLicense = new com.aspose.diagram.License();
 		cadLicense = new com.aspose.cad.License();
-
-		try (InputStream is = new ByteArrayInputStream(lic.getBytes());
-		     InputStream is2 = new ByteArrayInputStream(lic.getBytes());
-		     InputStream is3 = new ByteArrayInputStream(lic.getBytes());
-		     InputStream is4 = new ByteArrayInputStream(lic.getBytes());
-		     InputStream is5 = new ByteArrayInputStream(lic.getBytes());
-		     InputStream is6 = new ByteArrayInputStream(lic.getBytes());
-		     InputStream is7 = new ByteArrayInputStream(lic.getBytes());
-		     InputStream is8 = new ByteArrayInputStream(lic.getBytes());
-		     InputStream is9 = new ByteArrayInputStream(lic.getBytes());
-		     InputStream is10 = new ByteArrayInputStream(lic.getBytes())) {
-
-			emailLicense.setLicense(is);
-			slidesLicense.setLicense(is2);
-			cellsLicense.setLicense(is3);
-			wordsLicense.setLicense(is4);
-			pdfLicense.setLicense(is5);
-			imgLicense.setLicense(is6);
-			tasksLicense.setLicense(is7);
-			ocrLicense.setLicense(is8);
-			cadLicense.setLicense(is9);
-			diagramLicense.setLicense(is10);
-		} catch (Exception ex) {
+		logger.debug("Getting the input streams...");
+		try {
+			logger.debug("Setting the licences");
+			emailLicense.setLicense(tmpFileFullPathStr);
+			slidesLicense.setLicense(tmpFileFullPathStr);
+			cellsLicense.setLicense(tmpFileFullPathStr);
+			wordsLicense.setLicense(tmpFileFullPathStr);
+			pdfLicense.setLicense(tmpFileFullPathStr);
+			imgLicense.setLicense(tmpFileFullPathStr);
+			tasksLicense.setLicense(tmpFileFullPathStr);
+			logger.debug("About to do OCR licence");
+			ocrLicense.setLicense(tmpFileFullPathStr);
+			logger.debug("OCR licence valid is: " + ocrLicense.isValid());
+			cadLicense.setLicense(tmpFileFullPathStr);
+			diagramLicense.setLicense(tmpFileFullPathStr);
+			Files.deleteIfExists(tmpFileFullPath);
+		}
+		catch (Exception ex) {
 			logger.error("An error occurred while loading in an Aspose license.", ex);
 		}
 
@@ -108,23 +109,5 @@ public final class LicenseHelper {
 
 	public static LicenseHelper getLicenseHelper() {
 		return licenseHelper;
-	}
-	
-	public static void licenseReader(Path licFilePath) {
-		List<String> licLines;
-		try {
-			licLines = Files.readAllLines(licFilePath);
-			System.out.println("\tprivate static final String lic = \"<?xml version=\\\"1.0\\\" encoding=\\\"utf-8\\\"?>\\n\" +");
-			for (String licLine : licLines) {
-				if (!licLine.startsWith("</License>")) {
-					System.out.println("\t\t\t\"" + licLine.trim() + "\\n\" +");
-				}
-				else {
-					System.out.println("\t\t\t\"" + licLine.trim() + "\";");
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
