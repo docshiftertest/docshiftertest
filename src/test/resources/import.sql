b6c85c728017062de728f0fcf9633c2d4ee7864c
Juan Marques <juan.marques@docshifter.com>
2020-12-28 20:13:32 +0100

    Changed test url to fix failing tests.

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,1 +1,1 @@
-INSERT INTO GLOBAL_SETTINGS (DEFAULT_ERROR_FOLDER, DEFAULT_TEMP_FOLDER, MQ_QUEUE, MQ_SYSTEM, MQURL, MQ_USER, MQ_USER_PASSWORD) VALUES ('target/test-classes/ds/error', 'target/test-classes/ds/work', 'docshifter', 'amqp', '192.168.10.138', 'docshifter', 'docshifter');
+INSERT INTO GLOBAL_SETTINGS (DEFAULT_ERROR_FOLDER, DEFAULT_TEMP_FOLDER, MQ_QUEUE, MQ_SYSTEM, MQURL, MQ_USER, MQ_USER_PASSWORD) VALUES ('target/test-classes/ds/error', 'target/test-classes/ds/work', 'docshifter', 'amqp', 'tcp://192.168.10.138:61616', 'docshifter', 'docshifter');
