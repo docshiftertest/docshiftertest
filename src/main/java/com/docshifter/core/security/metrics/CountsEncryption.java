155304dee3358ce2765e329a12fbc0c7f6ec07d5
Boyan Dunev <boyan.dunev@docshifter.com>
2021-03-30 13:25:09 +0200

    Remove grey code

--- src/main/java/com/docshifter/core/security/metrics/CountsEncryption.java
+++ src/main/java/com/docshifter/core/security/metrics/CountsEncryption.java
@@ -1,40 +1,27 @@
 package com.docshifter.core.security.metrics;
 
-import org.apache.commons.codec.binary.Base64;
-import org.apache.log4j.Logger;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.security.GeneralSecurityException;
-import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Arrays;
-
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 /**
  * Class to handle the task of encrypting the counts file.
  *
- * @author Boyan Dunev
+ * @author Boyan Dunev, 25-03-2021
  *  Following the guide at: https://mkyong.com/java/java-asymmetric-cryptography-example/
  *
  */
 public class CountsEncryption {
     private Cipher cipher;
-
-    private static final Logger logger = Logger.getLogger(new Object() {
-    }.getClass().getEnclosingClass());
 
     public CountsEncryption() throws NoSuchAlgorithmException, NoSuchPaddingException {
         this.cipher = Cipher.getInstance("RSA");
@@ -57,7 +44,7 @@ public class CountsEncryption {
 
     //Writes a byte array to a file
     public void writeToFile(File output, byte[] toWrite)
-            throws IllegalBlockSizeException, BadPaddingException, IOException {
+            throws  IOException {
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream(128);
                 FileOutputStream fos = new FileOutputStream(output)){
             baos.write(toWrite);
