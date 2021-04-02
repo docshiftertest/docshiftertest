3916905152741e45fe67e07a584a16b9f97387fa
Johnno Van de Velde <johnno.vandevelde@docshifter.com>
2021-04-02 21:40:28 +0200

    Improve and add utils for PDF

--- src/main/java/com/docshifter/core/asposehelper/utils/pdf/AnnotationUtils.java
+++ src/main/java/com/docshifter/core/asposehelper/utils/pdf/AnnotationUtils.java
@@ -1,51 +1,138 @@
 package com.docshifter.core.asposehelper.utils.pdf;
 
-import com.aspose.pdf.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.aspose.pdf.Document;
+import com.aspose.pdf.ExplicitDestination;
+import com.aspose.pdf.GoToAction;
+import com.aspose.pdf.IAppointment;
+import com.aspose.pdf.IDocument;
+import com.aspose.pdf.LinkAnnotation;
+import com.aspose.pdf.NamedDestination;
+import com.aspose.pdf.Page;
+import com.aspose.pdf.XYZExplicitDestination;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public final class AnnotationUtils {
-	private static final Logger logger = LoggerFactory.getLogger(AnnotationUtils.class);
-
 	private AnnotationUtils() {}
 
-	public static ExplicitDestination extractExplicitDestination(LinkAnnotation annotation) {
+	/**
+	 * Unwraps an {@link IAppointment} from a {@link LinkAnnotation} entirely and tries to cast the result as an
+	 * {@link ExplicitDestination}. Will return null if it is not an {@link ExplicitDestination}.
+	 * @param doc The document containing the {@link LinkAnnotation} and {@link NamedDestination}s.
+	 * @param annotation The object to extract the destination from.
+	 * @return The retrieved {@link ExplicitDestination} or null if it couldn't be casted or retrieved.
+	 */
+	public static ExplicitDestination extractExplicitDestinationSoft(IDocument doc, LinkAnnotation annotation) {
 		IAppointment annotationDest = annotation.getDestination();
 
 		// If destination on the annotation itself is null,
-		// we might have to deal with a GoToAction wrapping a XYZExplicitDestination here
+		// we might have to deal with a GoToAction wrapping a ExplicitDestination here
 		if (annotationDest == null) {
-			PdfAction outlineAction = annotation.getAction();
-			if (outlineAction instanceof GoToAction) {
-				annotationDest = ((GoToAction)outlineAction).getDestination();
-			}
+			annotationDest = annotation.getAction();
 		}
 
-		// Check if destination is of right type
-		if (!(annotationDest instanceof ExplicitDestination)) {
-			if (annotationDest != null) {
-				logger.debug(annotationDest + " was not a ExplicitDestination: skipping. It's a "
-						+ annotationDest.getClass().getName());
-			}
-			return null;
+		ExplicitDestination dest = AppointmentUtils.asExplicitDestinationSoft(doc, annotationDest);
+		if (dest != null) {
+			log.debug("Appropriate link found referring to page {}", dest.getPageNumber());
 		}
-
-		ExplicitDestination dest = (ExplicitDestination)annotationDest;
-		logger.debug("Appropriate link found referring to page " + dest.getPageNumber());
 		return dest;
 	}
 
+	/**
+	 * Unwraps a {@link LinkAnnotation} from an {@link IAppointment} (specifically {@link GoToAction}s) entirely and
+	 * tries to cast the result as an {@link ExplicitDestination}. Will return null if it is not an
+	 * {@link ExplicitDestination}. ONLY use this if you don't care about following {@link NamedDestination}s! For
+	 * more robust checking see {@link #extractExplicitDestinationSoft(IDocument, LinkAnnotation)}.
+	 * @param annotation The object to extract the destination from.
+	 * @return The retrieved {@link ExplicitDestination} or null if it couldn't be casted or retrieved.
+	 */
+	public static ExplicitDestination extractExplicitDestinationHard(LinkAnnotation annotation) {
+		IAppointment annotationDest = annotation.getDestination();
+
+		// If destination on the annotation itself is null,
+		// we might have to deal with a GoToAction wrapping a ExplicitDestination here
+		if (annotationDest == null) {
+			annotationDest = annotation.getAction();
+		}
+
+		ExplicitDestination dest = AppointmentUtils.asExplicitDestinationHard(annotationDest);
+		if (dest != null) {
+			log.debug("Appropriate link found referring to page {}", dest.getPageNumber());
+		}
+		return dest;
+	}
+
+	@Deprecated
+	public static ExplicitDestination extractExplicitDestination(LinkAnnotation annotation) {
+		return extractExplicitDestinationHard(annotation);
+	}
+
+	@Deprecated
 	public static void setDestinationOrAction(LinkAnnotation annotation, ExplicitDestination dest) {
-		if (annotation.getDestination() != null) {
+		setDestinationOrActionHard(annotation, dest);
+	}
+
+	public static void setDestinationOrActionHard(LinkAnnotation annotation, ExplicitDestination dest) {
+		if (annotation.getDestination() != null || annotation.getAction() == null) {
 			annotation.setDestination(dest);
 		} else {
 			annotation.setAction(new GoToAction(dest));
 		}
 	}
 
-	public static XYZExplicitDestination setDestinationToUpperLeftCorner(LinkAnnotation annotation, Page target) {
+	public static void setDestinationOrActionHard(LinkAnnotation annotation, Document doc, NamedDestination dest) {
+		if (annotation.getDestination() != null || annotation.getAction() == null) {
+			annotation.setDestination(dest);
+		} else {
+			annotation.setAction(new GoToAction(doc, dest.getName()));
+		}
+	}
+
+	public static void setDestinationOrActionSoft(Document doc, LinkAnnotation annotation,
+												  ExplicitDestination dest) {
+		if (annotation.getDestination() != null) {
+			AppointmentUtils.replaceIfWrapped(doc, annotation.getDestination(), dest);
+		} else if (annotation.getAction() != null) {
+			if (annotation.getAction() instanceof GoToAction) {
+				AppointmentUtils.replaceIfWrapped(doc, annotation.getAction(), dest);
+			} else {
+				annotation.setAction(new GoToAction(dest));
+			}
+		} else {
+			annotation.setDestination(dest);
+		}
+	}
+
+	public static void setDestinationOrActionSoft(Document doc, LinkAnnotation annotation,
+												  NamedDestination dest) {
+		if (annotation.getDestination() != null) {
+			AppointmentUtils.replaceIfWrapped(doc, annotation.getDestination(), dest);
+		} else if (annotation.getAction() != null) {
+			if (annotation.getAction() instanceof GoToAction) {
+				AppointmentUtils.replaceIfWrapped(doc, annotation.getAction(), dest);
+			} else {
+				annotation.setAction(new GoToAction(doc, dest.getName()));
+			}
+		} else {
+			annotation.setDestination(dest);
+		}
+	}
+
+	public static XYZExplicitDestination setDestinationToUpperLeftCornerSoft(Document doc,
+																			 LinkAnnotation annotation, Page target) {
 		XYZExplicitDestination dest = XYZExplicitDestination.createDestinationToUpperLeftCorner(target);
-		setDestinationOrAction(annotation, dest);
+		setDestinationOrActionSoft(doc, annotation, dest);
 		return dest;
+	}
+
+	public static XYZExplicitDestination setDestinationToUpperLeftCornerHard(LinkAnnotation annotation, Page target) {
+		XYZExplicitDestination dest = XYZExplicitDestination.createDestinationToUpperLeftCorner(target);
+		setDestinationOrActionHard(annotation, dest);
+		return dest;
+	}
+
+	@Deprecated
+	public static XYZExplicitDestination setDestinationToUpperLeftCorner(LinkAnnotation annotation, Page target) {
+		return setDestinationToUpperLeftCornerHard(annotation, target);
 	}
 }
--- src/main/java/com/docshifter/core/asposehelper/utils/pdf/OutlineUtils.java
+++ src/main/java/com/docshifter/core/asposehelper/utils/pdf/OutlineUtils.java
@@ -1,37 +1,64 @@
 package com.docshifter.core.asposehelper.utils.pdf;
 
-import com.aspose.pdf.*;
+import com.aspose.pdf.Document;
+import com.aspose.pdf.ExplicitDestination;
+import com.aspose.pdf.GoToAction;
+import com.aspose.pdf.IAppointment;
+import com.aspose.pdf.IDocument;
+import com.aspose.pdf.NamedDestination;
+import com.aspose.pdf.OutlineItemCollection;
+import com.aspose.pdf.Outlines;
+import com.aspose.pdf.Page;
+import com.aspose.pdf.XYZExplicitDestination;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public final class OutlineUtils {
 	private OutlineUtils() {}
 
-	public static ExplicitDestination extractExplicitDestination(OutlineItemCollection outline) {
+	/**
+	 * Get
+	 * @param doc
+	 * @param outline
+	 * @return
+	 */
+	public static ExplicitDestination extractExplicitDestinationSoft(IDocument doc, OutlineItemCollection outline) {
 		IAppointment outlineDest = outline.getDestination();
 
 		// If destination on the outline itself is null,
-		// we might have to deal with a GoToAction wrapping a XYZExplicitDestination here
+		// we might have to deal with a GoToAction wrapping a ExplicitDestination here
 		if (outlineDest == null) {
-			PdfAction outlineAction = outline.getAction();
-			if (outlineAction instanceof GoToAction) {
-				outlineDest = ((GoToAction)outlineAction).getDestination();
-			}
+			outlineDest = outline.getAction();
 		}
 
-		// Check if destination is of right type
-		if (!(outlineDest instanceof ExplicitDestination)) {
-			if (outlineDest != null) {
-				log.debug("{} was not a ExplicitDestination: skipping. It's a {}", outlineDest,
-						outlineDest.getClass().getName());
-			}
-			return null;
+		ExplicitDestination bookmark = AppointmentUtils.asExplicitDestinationSoft(doc, outlineDest);
+		if (bookmark != null) {
+			log.debug("Appropriate level {} bookmark found referring to page {}", outline.getLevel(),
+					bookmark.getPageNumber());
 		}
-
-		ExplicitDestination bookmark = (ExplicitDestination)outlineDest;
-		log.debug("Appropriate level {} bookmark found referring to page {}", outline.getLevel(),
-				bookmark.getPageNumber());
 		return bookmark;
+	}
+
+	public static ExplicitDestination extractExplicitDestinationHard(OutlineItemCollection outline) {
+		IAppointment outlineDest = outline.getDestination();
+
+		// If destination on the outline itself is null,
+		// we might have to deal with a GoToAction wrapping a ExplicitDestination here
+		if (outlineDest == null) {
+			outlineDest = outline.getAction();
+		}
+
+		ExplicitDestination bookmark = AppointmentUtils.asExplicitDestinationHard(outlineDest);
+		if (bookmark != null) {
+			log.debug("Appropriate level {} bookmark found referring to page {}", outline.getLevel(),
+					bookmark.getPageNumber());
+		}
+		return bookmark;
+	}
+
+	@Deprecated
+	public static ExplicitDestination extractExplicitDestination(OutlineItemCollection outline) {
+		return extractExplicitDestinationHard(outline);
 	}
 
 	public static void moveOutline(OutlineItemCollection oldRoot, OutlineItemCollection newRoot) {
@@ -40,17 +67,72 @@ public final class OutlineUtils {
 		oldParent.remove(oldRoot);
 	}
 
+	@Deprecated
 	public static void setDestinationOrAction(OutlineItemCollection outline, ExplicitDestination dest) {
-		if (outline.getDestination() != null) {
+		setDestinationOrActionHard(outline, dest);
+	}
+
+	public static void setDestinationOrActionHard(OutlineItemCollection outline, ExplicitDestination dest) {
+		if (outline.getDestination() != null || outline.getAction() == null) {
 			outline.setDestination(dest);
 		} else {
 			outline.setAction(new GoToAction(dest));
 		}
 	}
 
-	public static XYZExplicitDestination setDestinationToUpperLeftCorner(OutlineItemCollection outline, Page target) {
+	public static void setDestinationOrActionHard(OutlineItemCollection outline, Document doc, NamedDestination dest) {
+		if (outline.getDestination() != null || outline.getAction() == null) {
+			outline.setDestination(dest);
+		} else {
+			outline.setAction(new GoToAction(doc, dest.getName()));
+		}
+	}
+
+	public static void setDestinationOrActionSoft(Document doc, OutlineItemCollection outline,
+												  ExplicitDestination dest) {
+		if (outline.getDestination() != null) {
+			AppointmentUtils.replaceIfWrapped(doc, outline.getDestination(), dest);
+		} else if (outline.getAction() != null) {
+			if (outline.getAction() instanceof GoToAction) {
+				AppointmentUtils.replaceIfWrapped(doc, outline.getAction(), dest);
+			} else {
+				outline.setAction(new GoToAction(dest));
+			}
+		} else {
+			outline.setDestination(dest);
+		}
+	}
+
+	public static void setDestinationOrActionSoft(Document doc, OutlineItemCollection outline,
+												  NamedDestination dest) {
+		if (outline.getDestination() != null) {
+			AppointmentUtils.replaceIfWrapped(doc, outline.getDestination(), dest);
+		} else if (outline.getAction() != null) {
+			if (outline.getAction() instanceof GoToAction) {
+				AppointmentUtils.replaceIfWrapped(doc, outline.getAction(), dest);
+			} else {
+				outline.setAction(new GoToAction(doc, dest.getName()));
+			}
+		} else {
+			outline.setDestination(dest);
+		}
+	}
+
+	public static XYZExplicitDestination setDestinationToUpperLeftCornerSoft(Document doc,
+																			 OutlineItemCollection outline, Page target) {
 		XYZExplicitDestination dest = XYZExplicitDestination.createDestinationToUpperLeftCorner(target);
-		setDestinationOrAction(outline, dest);
+		setDestinationOrActionSoft(doc, outline, dest);
 		return dest;
+	}
+
+	public static XYZExplicitDestination setDestinationToUpperLeftCornerHard(OutlineItemCollection outline, Page target) {
+		XYZExplicitDestination dest = XYZExplicitDestination.createDestinationToUpperLeftCorner(target);
+		setDestinationOrActionHard(outline, dest);
+		return dest;
+	}
+
+	@Deprecated
+	public static XYZExplicitDestination setDestinationToUpperLeftCorner(OutlineItemCollection outline, Page target) {
+		return setDestinationToUpperLeftCornerHard(outline, target);
 	}
 }
