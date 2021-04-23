4f80b1000194d93b93d7d2944c76ab983f29cf66
Johnno Van de Velde <johnno.vandevelde@docshifter.com>
2021-04-23 17:39:37 +0200

    Refactor to multiple ExecutorServices

--- src/main/java/com/docshifter/core/config/MonitoringConfiguration.java
+++ src/main/java/com/docshifter/core/config/MonitoringConfiguration.java
@@ -2,8 +2,10 @@ package com.docshifter.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -17,8 +19,21 @@ public class MonitoringConfiguration {
 		return new RestTemplate();
 	}
 
-	@Bean
-	public ScheduledExecutorService executorService() {
+	/**
+	 * ExecutorService that dynamically spawns and reuses threads as needed, recommended for general-purpose tasks.
+	 */
+	@Bean(name = "generalES")
+	@Primary
+	public ExecutorService executorService() {
+		return Executors.newCachedThreadPool();
+	}
+
+	/**
+	 * ExecutorService that supports scheduling backed by a fixed number of threads, exclusively for lightweight and
+	 * pure CPU-bound tasks.
+	 */
+	@Bean(name = "lightweightScheduledES")
+	public ScheduledExecutorService scheduledExecutorService() {
 		return Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
 	}
 
