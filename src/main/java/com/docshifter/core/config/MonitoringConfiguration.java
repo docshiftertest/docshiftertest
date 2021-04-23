21a4f4ef9624cb5932cb7f1a995a2178ceec397f
Johnno Van de Velde <johnno.vandevelde@docshifter.com>
2021-04-23 15:49:34 +0200

    General application stability improvements (PART 1, CHANGES FOR PF ONLY):
    - Switch to ScheduledExecutorService bean instead of normal one which does not support scheduling/delaying tasks
    - It makes sense to only do the licensing pod check at application startup and improves stability in case the Kubernetes master is temporarily unavailable

--- src/main/java/com/docshifter/core/config/MonitoringConfiguration.java
+++ src/main/java/com/docshifter/core/config/MonitoringConfiguration.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * Created by blazejm on 30.05.2017.
@@ -18,8 +18,8 @@ public class MonitoringConfiguration {
 	}
 
 	@Bean
-	public ExecutorService executorService() {
-		return Executors.newCachedThreadPool();
+	public ScheduledExecutorService executorService() {
+		return Executors.newScheduledThreadPool(Runtime.getRuntime().availableProcessors());
 	}
 
 }
