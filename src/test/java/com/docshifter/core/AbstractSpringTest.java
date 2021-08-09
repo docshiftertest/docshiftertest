6f16f5cf40242d298511c27ecbbc8dbe4b949ef4
julian-ds <julian.isaac@docshifter.com>
2021-08-09 16:07:52 +0200

    Try Mocking the Bean

--- src/test/java/com/docshifter/core/AbstractSpringTest.java
+++ src/test/java/com/docshifter/core/AbstractSpringTest.java
@@ -1,7 +1,9 @@
 package com.docshifter.core;
 
+import com.docshifter.core.config.services.ILicensingService;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
@@ -11,5 +13,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BeansApplication.class)
 public abstract class AbstractSpringTest {
+
+    @MockBean
+    protected ILicensingService nalpeironService;
 
 }
