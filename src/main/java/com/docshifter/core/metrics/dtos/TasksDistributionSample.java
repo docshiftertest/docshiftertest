672e950de882fc3d3e045761016d5416744e2de5
Juan Marques <juan.marques@docshifter.com>
2021-08-13 17:28:42 +0200

    Adding Task distribution sample class of Dashboard.

---
+++ src/main/java/com/docshifter/core/metrics/dtos/TasksDistributionSample.java
@@ -0,0 +1,10 @@
+package com.docshifter.core.metrics.dtos;
+
+/**
+ * Sample class of Dashboard Entity
+ * @author Created by Juan Marques on 03/08/2021
+ */
+public interface TasksDistributionSample {
+
+    Long getFinishTimestamp();
+}
--- src/main/java/com/docshifter/core/metrics/repositories/DashboardRepository.java
+++ src/main/java/com/docshifter/core/metrics/repositories/DashboardRepository.java
@@ -1,13 +1,16 @@
 package com.docshifter.core.metrics.repositories;
 
+import com.docshifter.core.metrics.dtos.TasksDistributionSample;
 import com.docshifter.core.metrics.entities.Dashboard;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /**
  * Created by Julian Isaac on 02.08.2021
  */
 public interface DashboardRepository extends JpaRepository<Dashboard, String> {
 
-// Will maybe add some Queries here ...
+    List<TasksDistributionSample> getAllBy();
 
 }
