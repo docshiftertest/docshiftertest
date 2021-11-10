0059afa56ed69fd89116cc63707d8c1da59aef5f
Johnno Van de Velde <johnno.vandevelde@docshifter.com>
2021-11-10 15:22:10 +0100

    CodeGuru improvement

--- src/main/java/com/docshifter/core/utils/HashUtils.java
+++ src/main/java/com/docshifter/core/utils/HashUtils.java
@@ -66,19 +66,16 @@ public final class HashUtils {
 
 	private static String getFileChecksum(MessageDigest digest, File file) throws IOException {
 		//Get file input stream for reading the file content
-		FileInputStream fis = new FileInputStream(file);
+		try (FileInputStream fis = new FileInputStream(file)) {
+			//Create byte array to read data in chunks
+			byte[] byteArray = new byte[1024];
+			int bytesCount;
 
-		//Create byte array to read data in chunks
-		byte[] byteArray = new byte[1024];
-		int bytesCount;
-
-		//Read file data and update in message digest
-		while ((bytesCount = fis.read(byteArray)) != -1) {
-			digest.update(byteArray, 0, bytesCount);
+			//Read file data and update in message digest
+			while ((bytesCount = fis.read(byteArray)) != -1) {
+				digest.update(byteArray, 0, bytesCount);
+			}
 		}
-
-		//close the stream; We don't need it now.
-		fis.close();
 
 		//Get the hash's bytes
 		byte[] bytes = digest.digest();
