6e4c2629f0a1c6a17095564d96c04a39e1d4670a
Johnno Van de Velde <johnno.vandevelde@docshifter.com>
2021-11-10 14:56:10 +0100

    Move common hashing functionality to beans, add tests and documentation, add Hash Snapshot module to LicensableModule and to the appropriate sets

---
+++ src/main/java/com/docshifter/core/utils/HashUtils.java
@@ -0,0 +1,96 @@
+package com.docshifter.core.utils;
+
+import lombok.extern.log4j.Log4j2;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+/**
+ * Provides common methods for hashing (for integrity checking).
+ */
+@Log4j2
+public final class HashUtils {
+	private static final Pattern DIGEST_PATTERN = Pattern.compile("[\\s\\-]");
+	private static final Map<String, String> digestMap = fillDigestMap("MD5", "SHA-1", "SHA-256", "SHA-512");
+
+	private HashUtils() {}
+
+	private static Map<String, String> fillDigestMap(String... digests) {
+		return Arrays.stream(digests).collect(
+				Collectors.toMap(digest -> DIGEST_PATTERN.matcher(digest).replaceAll("").toLowerCase(),
+				digest -> digest));
+	}
+
+	/**
+	 * Calculates the hash of a file located at a Path.
+	 * @param inFilePath The Path to check.
+	 * @param digestMethod The digest method to use. MD5 and SHA variants are supported.
+	 * @return The calculated hash for the provided file. Null if the file couldn't be read or if the digest method is
+	 * invalid.
+	 */
+	public static String calculateHash(Path inFilePath, String digestMethod) {
+
+		String digestMethodToUse;
+
+		if (StringUtils.isBlank(digestMethod)) {
+			log.error("digestMethod cannot be NULL or empty! Returning NULL hash...");
+			return null;
+		}
+
+		digestMethod = DIGEST_PATTERN.matcher(digestMethod).replaceAll("").toLowerCase();
+		digestMethodToUse = digestMap.get(digestMethod);
+		if (digestMethodToUse == null) {
+			log.error("digestMethod is not available: {}", digestMethod);
+			return null;
+		}
+
+		try {
+			MessageDigest digest = MessageDigest.getInstance(digestMethodToUse);
+			return getFileChecksum(digest, inFilePath.toFile());
+		}
+		catch (IOException | NoSuchAlgorithmException e) {
+			log.error(e.getClass().getSimpleName() + ": " + digestMethod, e);
+		}
+
+		return null;
+	}
+
+	private static String getFileChecksum(MessageDigest digest, File file) throws IOException {
+		//Get file input stream for reading the file content
+		FileInputStream fis = new FileInputStream(file);
+
+		//Create byte array to read data in chunks
+		byte[] byteArray = new byte[1024];
+		int bytesCount;
+
+		//Read file data and update in message digest
+		while ((bytesCount = fis.read(byteArray)) != -1) {
+			digest.update(byteArray, 0, bytesCount);
+		}
+
+		//close the stream; We don't need it now.
+		fis.close();
+
+		//Get the hash's bytes
+		byte[] bytes = digest.digest();
+
+		//This bytes[] has bytes in decimal format;
+		//Convert it to hexadecimal format
+		StringBuilder sb = new StringBuilder();
+		for (byte aByte : bytes) {
+			sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+		}
+
+		//return complete hash
+		return sb.toString();
+	}
+}
---
+++ src/test/java/com/docshifter/core/utils/HashUtilsTest.java
@@ -0,0 +1,44 @@
+package com.docshifter.core.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.Paths;
+
+public class HashUtilsTest {
+	@Test
+	public void calculateHash_md5() {
+		String hash = HashUtils.calculateHash(Paths.get("target/test-classes/attachment.txt"), "md5");
+		Assert.assertEquals("4d4d5e12d2e5d2394543d18653b6ccc7", hash);
+	}
+
+	@Test
+	public void calculateHash_sha1() {
+		String hash = HashUtils.calculateHash(Paths.get("target/test-classes/attachment.txt"), "sha 1");
+		Assert.assertEquals("9db729bede8d46beb96945b11269f4849e1f96a9", hash);
+	}
+
+	@Test
+	public void calculateHash_sha256() {
+		String hash = HashUtils.calculateHash(Paths.get("target/test-classes/attachment.txt"), "sHa256");
+		Assert.assertEquals("8634a7c49a935d848e3b912e9dcdf22ec3ce67797ab4b6dda1f1368eeb8f6181", hash);
+	}
+
+	@Test
+	public void calculateHash_sha512() {
+		String hash = HashUtils.calculateHash(Paths.get("target/test-classes/attachment.txt"), "shA- 512");
+		Assert.assertEquals("8882e3133701e3c818714f625066e8320acf0edc561736ef008d13f5d4ae37b1ac152fede929793bf448b7aff223faa322f5ccfd0171dca13618380bc04a05c7", hash);
+	}
+
+	@Test
+	public void calculateHash_invalidPath() {
+		String hash = HashUtils.calculateHash(Paths.get("target/test-classes/idonot.exist"), "md5");
+		Assert.assertNull(hash);
+	}
+
+	@Test
+	public void calculateHash_invalidDigestMethod() {
+		String hash = HashUtils.calculateHash(Paths.get("target/test-classes/attachment.txt"), "whatever");
+		Assert.assertNull(hash);
+	}
+}
