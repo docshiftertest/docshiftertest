ee2c1959dbf2a7042387d5bd6d529a351e056527
Johnno Van de Velde <johnno.vandevelde@docshifter.com>
2022-02-10 15:58:31 +0100

    Add Javadocs

--- src/main/java/com/docshifter/core/logging/appenders/TaskMessageAppender.java
+++ src/main/java/com/docshifter/core/logging/appenders/TaskMessageAppender.java
@@ -21,6 +21,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Log4j2 Appender plugin that automatically adds messages to a task based on log statements.
+ */
 @Plugin(name = "TaskMessage",
 		category = Core.CATEGORY_NAME,
 		elementType = Appender.ELEMENT_TYPE)
@@ -50,15 +53,30 @@ public class TaskMessageAppender extends AbstractAppender {
 		return new TaskMessageAppender(name, filter, layout, ignoreExceptions, properties);
 	}
 
+	/**
+	 * Adds a task to be tracked and associates the current/calling thread with it. Should preferably be called once
+	 * whenever a task is initially created.
+	 * @param task The task to start tracking.
+	 */
 	public static void trackTask(Task task) {
 		registerCurrentThread(task);
 		tasks.put(task.getId(), task);
 	}
 
+	/**
+	 * Associates the current/calling thread with a particular task. Should be called inside a new thread whenever one
+	 * is spawned in a module or wherever there is a task context.
+	 * @param task The task to create an association with.
+	 */
 	public static void registerCurrentThread(Task task) {
 		ThreadContext.put("taskId", task.getId());
 	}
 
+	/**
+	 * Removes a task from the tracking map based on whatever task is associated with the current thread, and also
+	 * gets rid of this association. Should be called once whenever a task is about to become irrelevant (whenever it
+	 * goes out of scope for garbage collection).
+	 */
 	public static void untrackTask() {
 		String taskId = ThreadContext.get("taskId");
 		if (taskId != null) {
@@ -67,6 +85,10 @@ public class TaskMessageAppender extends AbstractAppender {
 		}
 	}
 
+	/**
+	 * Gets called whenever a log event happens, in which case we should add a message to the task if appropriate.
+	 * @param logEvent The supplied log event by Log4j2.
+	 */
 	@Override
 	public void append(LogEvent logEvent) {
 		TaskMessageSeverity severity = logLevelMappings.get(logEvent.getLevel());
