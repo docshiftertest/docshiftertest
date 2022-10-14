eeebb9b91993eb32de7312f8a64380a8175612e3
Juan Marques <juan.marques@docshifter.com>
2022-10-14 21:40:14 +0200

    chore: Change audit application columns and add jsonb type

--- src/main/java/com/docshifter/core/audit/entities/ApplicationAudit.java
+++ src/main/java/com/docshifter/core/audit/entities/ApplicationAudit.java
@@ -1,10 +1,15 @@
 package com.docshifter.core.audit.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -16,16 +21,19 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ApplicationAudit implements Serializable {
 
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
-
     private String oldValue;
     private String newValue;
     private String username;
     private long eventDateTime;
-    private String propertyName;
-
+    private String eventType;
+    private String eventAction;
+    @Type(type = "com.vladmihalcea.hibernate.type.json.JsonType")
+    @Column(columnDefinition = "jsonb")
+    private Object details;
 }
