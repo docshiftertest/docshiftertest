728b920145d18a776e8f8afd6174397678a3b55b
Raphael Silva <raphael.silva@docshifter.com>
2022-10-14 00:15:02 -0300

    Removed AuditInfo.java

--- src/main/java/com/docshifter/core/audit/entities/ApplicationAudit.java
+++ src/main/java/com/docshifter/core/audit/entities/ApplicationAudit.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.Serializable;
 import java.util.UUID;
 
 @Entity
@@ -15,7 +16,7 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ApplicationAudit implements AuditInfo {
+public class ApplicationAudit implements Serializable {
 
     @Id
     @GeneratedValue(generator = "UUID")
@@ -27,53 +28,4 @@ public class ApplicationAudit implements AuditInfo {
     private long eventDateTime;
     private String propertyName;
 
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
-
-    @Override
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public long getEventDateTime() {
-        return this.eventDateTime;
-    }
-
-    @Override
-    public void setEventDateTime(long eventDateTime) {
-        this.eventDateTime = eventDateTime;
-    }
-
-    @Override
-    public String getOldValue() {
-        return oldValue;
-    }
-
-    @Override
-    public void setOldValue(String oldValue) {
-        this.oldValue = oldValue;
-    }
-
-    @Override
-    public String getNewValue() {
-        return newValue;
-    }
-
-    @Override
-    public void setNewValue(String newValue) {
-        this.newValue = newValue;
-    }
-
-    @Override
-    public String getPropertyName() {
-        return propertyName;
-    }
-
-    @Override
-    public void setPropertyName(String propertyName) {
-        this.propertyName = propertyName;
-    }
 }
--- src/main/java/com/docshifter/core/audit/entities/WorkflowAudit.java
+++ src/main/java/com/docshifter/core/audit/entities/WorkflowAudit.java
@@ -7,13 +7,14 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.io.Serializable;
 import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
-public class WorkflowAudit implements AuditInfo {
+public class WorkflowAudit implements Serializable {
 
     @Id
     @GeneratedValue(generator = "UUID")
@@ -43,27 +44,5 @@ public class WorkflowAudit implements AuditInfo {
         this.setPropertyName(propertyName);
         this.setUsername(username);
     }
-
-    @Override
-    public String getUsername() {
-        return this.username;
-    }
-
-    @Override
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public long getEventDateTime() {
-        return this.eventDateTime;
-    }
-
-    @Override
-    public void setEventDateTime(long eventDateTime) {
-        this.eventDateTime = eventDateTime;
-    }
-
-
 
 }
