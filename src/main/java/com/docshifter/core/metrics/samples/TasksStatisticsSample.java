cefe065cbb17b5ce3853e3b4cea89ede7e16e0f5
Johnno Van de Velde <johnno.vandevelde@docshifter.com>
2022-11-21 11:45:39 +0100

    chore: document why count field is long but why other fields are nullable Long/Double in TasksStatisticsSample

--- src/main/java/com/docshifter/core/metrics/samples/TasksStatisticsSample.java
+++ src/main/java/com/docshifter/core/metrics/samples/TasksStatisticsSample.java
@@ -4,7 +4,9 @@ package com.docshifter.core.metrics.samples;
  * @author Created by Juan Marques on 19/08/2021
  */
 public interface TasksStatisticsSample {
+    // long because a count never returns null
     long getCount();
+    // All the other fields can be null if the SQL didn't collect any records during the aggregate query
     Long getSum();
     Long getMinimum();
     Long getMaximum();
