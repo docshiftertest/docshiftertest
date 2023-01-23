55f847b1d0503a5e55e45167e0c1d0dc1794bd81
diego.lourenco <diego.lourenco@docshifter.com>
2023-01-23 12:18:12 -0300

    feat: add DestinationZoom to reuse it in more modules

---
+++ src/main/java/com/docshifter/core/utils/zoom/DestinationZoom.java
@@ -0,0 +1,93 @@
+package com.docshifter.core.utils.zoom;
+
+import com.aspose.pdf.ExplicitDestination;
+import com.aspose.pdf.OutlineItemCollection;
+import com.docshifter.core.asposehelper.utils.pdf.ExplicitDestinationTransformer;
+import com.docshifter.core.asposehelper.utils.pdf.OutlineUtils;
+import com.docshifter.core.exceptions.InvalidConfigException;
+import org.apache.commons.lang3.EnumUtils;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+
+/**
+ * Special PDF zoom presets and an extra field to signal that zoom values should not be changed.
+ */
+public enum DestinationZoom {
+
+    /**
+     * Don't change the zoom preset/value at all. Note: this just means the explicit destinations stored in the PDF
+     * document should be kept as is! It does not mean "inherit zoom" as that is not even a preset at all but simply
+     * a custom zoom value that is set to "0".
+     */
+    KEEP_AS_IS,
+
+    /**
+     * Fit the entire page inside the view.
+     */
+    FIT_PAGE,
+
+    /**
+     * Fit the entire width of the page inside the view.
+     */
+    FIT_WIDTH,
+
+    /**
+     * Fit what's visible on the page (a.k.a. the bounding box) inside the view.
+     */
+    FIT_VISIBLE;
+
+    /**
+     * Gets the function according to the zoomLevel provided
+     * @param zoomLevel the zoom level choice
+     * @return the function using as argument an {@link ExplicitDestinationTransformer} and as result an {@link ExplicitDestination}
+     * @throws InvalidConfigException throws it if it is not possible to convert the value provided
+     */
+    @Nullable
+    public static Function<ExplicitDestinationTransformer, ExplicitDestination> getFunction(String zoomLevel) throws InvalidConfigException {
+        Function<ExplicitDestinationTransformer, ExplicitDestination> operation;
+        DestinationZoom presetZoom = EnumUtils.getEnumIgnoreCase(DestinationZoom.class, zoomLevel);
+        if (presetZoom == null) {
+            try {
+                double customZoom = Double.parseDouble(zoomLevel);
+                operation = transformer -> transformer.toCustomZoom(customZoom);
+            }
+            catch (Exception exception) {
+                throw new InvalidConfigException("Tried to interpret value " + zoomLevel + " as custom zoom factor for " +
+                        "forceDestinationZoom but parsing of this value failed.", exception);
+            }
+        }
+        else {
+            operation = switch (presetZoom) {
+                case FIT_PAGE -> ExplicitDestinationTransformer::toFit;
+                case FIT_WIDTH -> ExplicitDestinationTransformer::toFitH;
+                case FIT_VISIBLE -> ExplicitDestinationTransformer::toFitBH;
+                default -> null;
+            };
+        }
+
+        return operation;
+    }
+
+    /**
+     * Updates the destination of the bookmarks
+     * @param outlines the {@link OutlineItemCollection} to iterate and update
+     * @param operation the function using as argument an {@link ExplicitDestinationTransformer} and as result an {@link ExplicitDestination}
+     */
+    public static void updateOutlineDestinations(Iterable<OutlineItemCollection> outlines,
+                                                  Function<ExplicitDestinationTransformer, ExplicitDestination> operation) {
+
+        for (OutlineItemCollection oic : outlines) {
+
+            ExplicitDestination dest = OutlineUtils.extractExplicitDestinationHard(oic);
+
+            if (dest != null) {
+                dest = operation.apply(ExplicitDestinationTransformer.create(dest));
+                oic.setDestination(dest);
+            }
+
+            updateOutlineDestinations(oic, operation);
+        }
+    }
+
+}
