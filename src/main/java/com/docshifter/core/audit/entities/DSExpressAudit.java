5b91b72f1d5c57c7d18bc2910f2d3e146a84aeb3
diego.lourenco <diego.lourenco@docshifter.com>
2023-05-25 14:36:02 +0000

    refactor: add AllArgsConstructor on DSExpressAudit

--- src/main/java/com/docshifter/core/audit/entities/DSExpressAudit.java
+++ src/main/java/com/docshifter/core/audit/entities/DSExpressAudit.java
@@ -1,5 +1,6 @@
 package com.docshifter.core.audit.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.io.Serializable;
 import java.util.UUID;
 
 @Entity
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
@@ -26,23 +28,4 @@ public class DSExpressAudit implements Serializable {
     private String oldValue;
     private String newValue;
     private String changeType;
-
-    public DSExpressAudit(UUID id,
-                          String dsexpressName,
-                          String propertyName,
-                          String changeType,
-                          String oldValue,
-                          String newValue,
-                          String username,
-                          long eventDateTime) {
-        this.id = id;
-        this.propertyName = propertyName;
-        this.dsexpressName = dsexpressName;
-        this.changeType = changeType;
-        this.oldValue = oldValue;
-        this.newValue = newValue;
-        this.username = username;
-        this.eventDateTime = eventDateTime;
-    }
-
 }
