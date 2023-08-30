60de47a7ebb5dd69994a53056b50b919d39ac8d5
Johnno Van de Velde <johnno.vandevelde@docshifter.com>
2023-08-30 18:50:29 +0200

    feat: Add new PdfToc utility class to search for Tables of Contents in a PDF document and to fetch their elements

---
+++ src/test/java/com/docshifter/core/asposehelper/utils/pdf/PdfTocTest.java
@@ -0,0 +1,27 @@
+package com.docshifter.core.asposehelper.utils.pdf;
+
+
+import com.aspose.pdf.Document;
+import com.docshifter.core.asposehelper.LicenseHelper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PdfTocTest {
+    @BeforeAll
+    static void beforeAll() {
+        LicenseHelper.getLicenseHelper();
+    }
+
+    @Test
+    void toxicology() {
+        try (Document doc = new Document("target/test-classes/266-toxicology-written.pdf")) {
+            PdfToc[] tocs = PdfToc.findAll(doc, "Contents", 0, 12, 60, 60)
+                    .toArray(PdfToc[]::new);
+            // TODO
+            assertEquals(1, tocs.length);
+            assertEquals(1, tocs[0].getElements().size());
+        }
+    }
+}
